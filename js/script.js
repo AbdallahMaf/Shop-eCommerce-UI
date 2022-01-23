9e2d4c97740397125ba50e0fb2f86811388642ff
Mohamed Abdallah Fall <abdallahfall2020@gmail.com>
2022-01-23 00:24:16 +0000

    second commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+bg = document.getElementsByClassName("bg");
+
+document.querySelectorAll(".product-link").forEach(item =>{
+    item.addEventListener("click", event =>{
+
+        id = item.getAttribute("data-productid");
+
+        productFull = document.getElementById( id );
+        bg[0].style.display = "block";
+
+        productFull.style.top = "50%"
+    })
+})
+
+document.addEventListener("keydown", function(event){
+    if(event.key === "Escape"){
+        bg[0].style.display = "none";
+    
+        document.querySelectorAll(".full-product ").forEach(productFull =>{
+            productFull.style.top = "-50%";
+        })
+    }
+})
